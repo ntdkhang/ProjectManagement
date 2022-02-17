e6baea8f538e16c7ce1c90450a56da898eed658b
Khang <khang.nguyen.trd@gmail.com>
2022-02-17 14:13:37 -0600

    Add award check

--- ProjectManagement/Views/AwardsView.swift
+++ ProjectManagement/Views/AwardsView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct AwardsView: View {
     static let tag: String? = "Awards"
     
+    @EnvironmentObject var dataController: DataController
+    @State private var selectedAward = Award.example
+    @State private var showingAwardDetails = false
+    
     var columns: [GridItem] {
         [GridItem(.adaptive(minimum: 100, maximum: 100))]
     }
@@ -19,25 +23,33 @@ struct AwardsView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(Award.allAwards) { award in
                         Button {
-                            
+                            selectedAward = award
+                            showingAwardDetails.toggle()
                         } label: {
                             Image(systemName: award.image)
                                 .resizable()
                                 .scaledToFit()
                                 .padding()
                                 .frame(width: 100, height: 100)
-                                .foregroundColor(Color.secondary.opacity(0.5))
+                                .foregroundColor(dataController.hasEarned(award: award) ?
+                                                 Color(award.color) : Color.secondary.opacity(0.5))
                         }
                     }
                 }
             }
             .navigationTitle("Awards")
+            .alert(alertTitle, isPresented: $showingAwardDetails, actions: {},
+                   message: { Text(selectedAward.description) })
         }
+    }
+    
+    var alertTitle: String {
+        dataController.hasEarned(award: selectedAward) ? "Unlocked: \(selectedAward.name)" : "Locked"
     }
 }
 
-struct AwardsView_Previews: PreviewProvider {
-    static var previews: some View {
-        AwardsView()
-    }
-}
+//struct AwardsView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        AwardsView()
+//    }
+//}
